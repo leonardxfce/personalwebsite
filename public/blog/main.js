870852dc4644b71e3e85a847ca98fd5227a3d962
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-06-28 00:02:34 -0300

    sumo eslint y modifico un poco la pagina del blog

--- public/blog/main.js
+++ public/blog/main.js
@@ -1,13 +1,12 @@
 const pegarEnBody = data => {
     var md = window.markdownit();
     var result = md.render(data);
-    $('body').html(result);
+    $('.container').html(result);
 };
 
 window.onload = function () {
     const config = {
         url: "articles/clase2.md"
     };
-    $.ajax(config)
-        .done(pegarEnBody);
+    $.ajax(config).done(pegarEnBody);
 }
